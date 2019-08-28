33471469052e52ac2b12bd841ada74e44a3a634d
MarkDrastrup <mark.drastrup@gmail.com>
2019-08-28 16:43:50 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import axios from 'axios';
+import HabitList from "./components/HabitList"
+import Navbar from "./components/Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid p-0 d-flex flex-column justify-content-between h-100">
+          <div className="row no-gutters">
+            <HabitList></HabitList>
+          </div>
+          <div className="row no-gutters">
+            <Navbar></Navbar>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    allHabits: state.allHabits,
+
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchHabits: async (habits) => {
+      console.log(habits)
+      dispatch({
+        type: "FETCH_HABITS",
+        data: habits
+      });
+    },
+  }
+};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
